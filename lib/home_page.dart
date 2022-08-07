a86b0f181d23a3a97ebd884718fa6fac724feb13
bath0ry <plylegacy@gmail.com>
2022-08-07 03:40:59 -0300

    criação profile e widgets appbar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:top_app_clone/profile_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,10 +8,41 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: GestureDetector(
-          onTap: () {},
-          child: Icon(Icons.person_outline),
+        title: Padding(
+          padding:
+              const EdgeInsets.only(left: 90, top: 80, bottom: 80, right: 50),
+          child: Image.network(
+              'https://whatsapp.boradetop.com.br/assets/images/bom/logo_top_preto.png'),
         ),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: ((context) => const ProfilePage())));
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Icon(
+              Icons.person_outline,
+              color: Color.fromARGB(255, 3, 224, 217),
+              size: 35,
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.notifications_none,
+                color: Color.fromARGB(255, 3, 224, 217),
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.help_outline,
+                color: Color.fromARGB(255, 3, 224, 217),
+              )),
+          SizedBox(width: 5)
+        ],
         backgroundColor: Color.fromARGB(255, 63, 62, 62),
       ),
     );
---
+++ lib/profile_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 63, 62, 62),
+      ),
+    );
+  }
+}
