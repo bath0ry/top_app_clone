1a0e4bc6ba4399d69d35275ecd23820fa3c23007
Paulo Gomes <plylegacy@gmail.com>
2022-08-16 11:15:53 -0300

    add app bar login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:top_app_clone/app_pages/home_page.dart';
+import 'package:top_app_clone/app_pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
