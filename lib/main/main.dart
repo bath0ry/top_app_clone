5e7973099744ca427016bbc8673d7de6eac5a680
Paulo Gomes <plylegacy@gmail.com>
2022-09-24 21:23:28 -0300

    add components folder/login wdgets/organizate

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:top_app_clone/login/view/login_page.dart';
-
+import 'package:top_app_clone/pages/view/login_page.dart';
+import 'package:top_app_clone/pages/view/login_page.dart';
 
 void main() {
   runApp(const MyApp());
